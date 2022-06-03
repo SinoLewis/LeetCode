4135af0f8ae50590ad96aa2572584e398829f287
Snow Den Code <92801609+SinoLewis@users.noreply.github.com>
2022-06-03 11:00:00 +0300

    Update py_2_ipynb_conv.py

--- py_2_ipynb_conv.py
+++ py_2_ipynb_conv.py
@@ -5,10 +5,11 @@ from os import listdir
 from os.path import isfile, join
 from pprint import pprint
 
-mypath = "/home/eren/Desktop/SnowCode/LeetCode/leetcode-solutions-qiyuangong/python"
+mypath = "<Your Python files directory to be converted>"
 
 async def converter(file):
-    os.system(f"/home/eren/.local/bin/p2j {mypath}/{file}")
+    # pip install p2j
+    os.system(f"p2j {mypath}/{file}")
     # subprocess.run("/home/eren/.local/bin/p2j", f"{mypath}/{file}")
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
