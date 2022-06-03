5da8ca01058ee8b71bdb337870c1b3f4a0d6f50e
SinoLewis <sinolewis@gmail.com>
2022-06-03 11:01:33 +0300

    Update py_2_ipynb_conv.py

--- py_2_ipynb_conv.py
+++ py_2_ipynb_conv.py
@@ -1,16 +1,13 @@
 import asyncio
-# import subprocess
 import os
 from os import listdir
 from os.path import isfile, join
-from pprint import pprint
 
 mypath = "<Your Python files directory to be converted>"
 
 async def converter(file):
     # pip install p2j
     os.system(f"p2j {mypath}/{file}")
-    # subprocess.run("/home/eren/.local/bin/p2j", f"{mypath}/{file}")
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
